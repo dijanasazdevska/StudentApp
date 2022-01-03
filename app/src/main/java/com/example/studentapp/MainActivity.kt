12e2485f671e05247384c70cb2405d633810dd83
dijanasazdevska <67866541+dijanasazdevska@users.noreply.github.com>
2022-01-03 14:10:54 +0100

    Added log out

--- app/src/main/java/com/example/studentapp/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/MainActivity.kt
@@ -64,7 +64,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_logout -> {
+              FirebaseAuth.getInstance().signOut()
+                navigateOut()
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
